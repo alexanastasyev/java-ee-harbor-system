36bb7b39fb544a196bd2670b13cd5686a29fd66a
TakkaTooo <maksim.astash@gmail.com>
2021-10-16 16:57:35 +0300

    JWA-8 Added queries to create base rows

---
+++ harbour_sql_scripts/insert_basic_rows.sql
@@ -0,0 +1,4 @@
+INSERT INTO harbour_roles (title) VALUES ('admin');
+INSERT INTO harbour_roles (title) VALUES ('captain');
+INSERT INTO harbour_roles (title) VALUES ('dispatcher');
+INSERT INTO harbour_users (login, password, role_id) VALUES ('admin', 'admin', 1);
