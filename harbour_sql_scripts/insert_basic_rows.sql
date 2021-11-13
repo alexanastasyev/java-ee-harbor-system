dbc66258176840cdcf40ff80252b7f35d4b4c9fe
alexanastasyev <alex.anastasyev@mail.ru>
2021-11-13 17:48:06 +0300

    JWA-9 Database scripts fixed 11/11

--- harbour_sql_scripts/insert_basic_rows.sql
+++ harbour_sql_scripts/insert_basic_rows.sql
@@ -1,4 +1,10 @@
 INSERT INTO harbour_roles (title) VALUES ('admin');
 INSERT INTO harbour_roles (title) VALUES ('captain');
 INSERT INTO harbour_roles (title) VALUES ('dispatcher');
-INSERT INTO harbour_users (login, password, role_id) VALUES ('admin', 'admin', 1);
+INSERT INTO harbour_roles (title) VALUES ('moderator');
+
+INSERT INTO statuses (title) VALUES ('active');
+INSERT INTO statuses (title) VALUES ('blocked');
+INSERT INTO statuses (title) VALUES ('deleted');
+
+INSERT INTO harbour_users (login, password, role_id, status_id) VALUES ('admin', 'admin', 1, 1);
